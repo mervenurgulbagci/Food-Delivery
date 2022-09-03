40df90e9b06aea6a759206cf39bc818a87699884
mervenurgulbagci <mervenurgulbagci>
2022-09-03 18:34:59 +0300

    Initial Commit

---
+++ Food-Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-Delivery
+//
+//  Created by Merve Nurgül BAĞCI on 3.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
